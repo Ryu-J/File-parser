151b05d2e0549544c6cb59f7a62af0abfc6641e2
justinh808 <justinmh@hawaii.edu>
2015-12-18 11:18:36 -1000

    Added Basic parsing
    hard coded
    parses for the word "ipsum" found in the test.txt file

--- scripts/read.js
+++ scripts/read.js
@@ -2,7 +2,7 @@ var inbuff = new FileReader();
 
 inbuff.onload = function() {
     var contents = inbuff.result;
-    document.getElementById("testP").innerHTML = "File contents: <br>" + contents;
+    parseText(contents);
 };
 
 inbuff.onerror = function() {
@@ -12,4 +12,19 @@ inbuff.onerror = function() {
 function getText() {
     var selectedFile = document.getElementById('inputfile').files[0];
     inbuff.readAsText(selectedFile);
-}
+};
+
+function parseText(ptext) {
+    "use strict";
+    var strings = ptext.split(" ");
+    var newString = "";
+    for (let element of strings) {
+        if (element.toLowerCase() === "ipsum") {
+                newString = newString.concat("<span style='color:red;'>" + element + "</span> ");
+        }
+        else {
+            newString = newString.concat(element + " ");
+        };
+    };
+    document.getElementById("testP").innerHTML = "File contents: <br>" + newString;
+};
