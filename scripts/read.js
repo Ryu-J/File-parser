31c5510afdcdb0ea9176129f7aa118c6db7f5573
justinh808 <justinmh@hawaii.edu>
2015-12-18 16:28:36 -1000

    Firefox bug
    Fixed the bug that existed if ran in firefox
    added comments

--- scripts/read.js
+++ scripts/read.js
@@ -1,3 +1,4 @@
+//  Creates the file reader buffer to read local files
 var inbuff = new FileReader();
 
 inbuff.onload = function() {
@@ -5,6 +6,7 @@ inbuff.onload = function() {
     var parseString = document.getElementById("parseString").value;
     parseString = parseString.trim();
     parseString = parseString.toLowerCase();
+    //  Only parse for a string if the user entered a value to parse for
     if (parseString.length > 0) {
         parseText(contents, parseString);
     }
@@ -17,7 +19,9 @@ inbuff.onerror = function() {
     alert("File could not be read!");
 };
 
-function getText() {
+//  Parses the text from the file the user chooses using the string the user chooses
+function parseFile() {
+    //  Only parse if the user chose a file
     if (document.getElementById('inputfile').files.length > 0) {
         var selectedFile = document.getElementById('inputfile').files[0];
         inbuff.readAsText(selectedFile);
@@ -27,20 +31,26 @@ function getText() {
     };
 };
 
+// Par
 function parseText(ptext, pstring) {
-    "use strict";
+    //  split the words into an array
     var strings = ptext.split(" ");
     var newString = "";
-    for (let element of strings) {
-        if (element.toLowerCase() === pstring) {
-                newString = newString.concat("<span style='color:red;'>" + element + "</span> ");
+    //  go through each word to check for the pstring
+    for (var i = 0; i < strings.length; i++) {
+        //  the current word is the same as pstring
+        if (strings[i].toLowerCase() === pstring) {
+                newString = newString.concat("<span style='color:red;'>" + strings[i] + "</span> ");
         }
+        //  the current word is not the same as pstring
         else {
-            newString = newString.concat(element + " ");
+            newString = newString.concat(strings[i] + " ");
         };
-        if (element.indexOf("\n") != -1) {
+        //  add extra newlines when a new line character is in the current word
+        if (strings[i].indexOf("\n") != -1) {
             newString = newString.concat("<br><br>");
         };
     };
+    //  output the results to the paragraph with id testP
     document.getElementById("testP").innerHTML = "File contents: <br>" + newString;
 };
