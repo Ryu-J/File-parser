3f96d3147eb91c24d1e9b9dc582d17141e72c4d3
justinh808 <justinmh@hawaii.edu>
2015-12-18 16:05:30 -1000

    Added input box for a parse-able string
    User can enter a string in the input box and the web app will parse the
    text file and highlight in red if that string exists in the text file.

--- scripts/read.js
+++ scripts/read.js
@@ -2,7 +2,15 @@ var inbuff = new FileReader();
 
 inbuff.onload = function() {
     var contents = inbuff.result;
-    parseText(contents);
+    var parseString = document.getElementById("parseString").value;
+    parseString = parseString.trim();
+    parseString = parseString.toLowerCase();
+    if (parseString.length > 0) {
+        parseText(contents, parseString);
+    }
+    else {
+        alert("Please enter a string to parse!");
+    };
 };
 
 inbuff.onerror = function() {
@@ -10,20 +18,28 @@ inbuff.onerror = function() {
 };
 
 function getText() {
-    var selectedFile = document.getElementById('inputfile').files[0];
-    inbuff.readAsText(selectedFile);
+    if (document.getElementById('inputfile').files.length > 0) {
+        var selectedFile = document.getElementById('inputfile').files[0];
+        inbuff.readAsText(selectedFile);
+    }
+    else {
+        alert("Please choose a file to parse!");
+    };
 };
 
-function parseText(ptext) {
+function parseText(ptext, pstring) {
     "use strict";
     var strings = ptext.split(" ");
     var newString = "";
     for (let element of strings) {
-        if (element.toLowerCase() === "ipsum") {
+        if (element.toLowerCase() === pstring) {
                 newString = newString.concat("<span style='color:red;'>" + element + "</span> ");
         }
         else {
             newString = newString.concat(element + " ");
+        };
+        if (element.indexOf("\n") != -1) {
+            newString = newString.concat("<br><br>");
         };
     };
     document.getElementById("testP").innerHTML = "File contents: <br>" + newString;
